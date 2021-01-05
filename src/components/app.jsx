d86f5bb7231146a55b5f1eabd5c2f142f16acdad
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2021-01-05 18:19:21 +0100

    update state with api giphy en dure

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,29 +1,59 @@
 // eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
 
 // eslint-disable-next-line no-unused-vars, react/prefer-stateless-function
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // liste des gifs
+      gifs: [],
+      //  le gif qui est en zoom à gauche
+      selectedGifId: "88iYsvbegSUn9bSTF8"
+    };
+
+    this.search("unicorn");
+  }
+
+  search = (query) => {
+    // on va appeler une api de GIF
+    giphy('3xBIkOP8X616bcGG1K65iNYZjzNQ8otm').search({
+      q: query,
+      rating: 'g',
+      limit: '9'
+    }, (error, result) => {
+      //  update the gig state with the info of the API
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
   render () {
-    const gifs = [
-      { id: "88iYsvbegSUn9bSTF8" },
-      { id: "3oEjHI8WJv4x6UPDB6" },
-      { id: "kC8N6DPOkbqWTxkNTe" },
-      { id: "3NtY188QaxDdC" }
-    ];
+    // const gifs = [
+    //   { id: "88iYsvbegSUn9bSTF8" },
+    //   { id: "3oEjHI8WJv4x6UPDB6" },
+    //   { id: "kC8N6DPOkbqWTxkNTe" },
+    //   { id: "3NtY188QaxDdC" }
+    // ];
+
+    // l'app va definir sa fonctionsearch bar dans le constructor et ensuite va la passer à son enfant la props search bar qui est une fonction.
 
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="88iYsvbegSUn9bSTF8" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class SearchBar extends Component {
+  handleUpdate = (event) => {
+    // aller fetch une API de gif et changer le state gifs
+    // log the value of the component
+    console.log(event.target.value);
+
+  }
+
   render() {
     return (
-      <input type="text" className="form-control form-search"/>
+      <input type="text" className="form-control form-search"
+      onChange={this.handleUpdate}/>
+      // ajouter l'event sur l'input pour afficher un gif à chaque lettre taper
     );
   }
 }
